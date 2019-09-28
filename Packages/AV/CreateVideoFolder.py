016b08bf27fe4a25847f3e000484985ce8f75283
foxman <lin.vladimir@gmail.com>
2019-09-28 23:07:01 +0800

    add Video Folder

---
+++ Packages/AV/CreateVideoFolder.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import getopt
+import time
+import requests
+import threading
+import gettext
+import shutil
+import mysql.connector
+from mysql.connector import Error
+import Actions
+import CIOS
+
+rootdir = os . path . dirname ( os . path . abspath ( __file__ ) )
+rootdir = rootdir + "/../../Templates/AV/Folder" ;
+rootdir = os . path . abspath ( rootdir )
+cwdir   = os . getcwd ( )
+
+for i in range ( 1 , len ( sys . argv ) ) :
+  name = sys . argv [ i ]
+  dname = cwdir + "/" + name
+  shutil . copytree ( rootdir , dname )

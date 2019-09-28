b671113870f2af1ec388b78a25a110837cbaf142
foxman <lin.vladimir@gmail.com>
2019-09-28 12:32:40 +0800

    add main problem

---
+++ CiosPy.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import getopt
+import time
+import requests
+import threading
+import mysql.connector
+from mysql.connector import Error
+import Actions
+import CIOS
+
+print("CIOS")

40fa5a17e90990ba8b0ddba5858a30dcac08f557
foxman <lin.vladimir@gmail.com>
2019-10-31 16:28:34 +0800

    Commit CiosPy

--- Packages/Voice/VoiceRecognition.py
+++ Packages/Voice/VoiceRecognition.py
@@ -13,66 +13,34 @@ import Actions
 import CIOS
 from   CIOS . Voice . Recognizer import Recognizer
 
-# R = Recognizer ( )
-# R . OpenMicrophone (   )
-# R . OpenMicrophone ( Device = 1 )
-# R . OpenMicrophone ( Device = 7 )
-
-# R . OpenFile ( "trying.wav" )
-# print ( R . Listen ( ) )
-
-# while True :
-#   R . OpenMicrophone ( Device = 1 )
-#   R . UseMicrophone ( )
-#   print ( R . Listen ( ) )
-
-# import speech_recognition as vrt
-
-# rtx = vrt . Recognizer ( )
-
-# mic = vrt . Microphone ( )
-# mic = vrt . Microphone ( device_index = 1 )
-# mic = vrt . Microphone ( device_index = 7 )
-
-# print ( vrt . Microphone . list_microphone_names ( ) )
-
-# hasSource = False
-
-# harvard = vrt . AudioFile ( 'trying.wav' )
-# with harvard as source:
-#   audio = rtx . record ( source )
-#   hasSource = True
-
-# with mic as source :
-#   audio = rtx . listen ( source )
-#   hasSource = True
-
-# if ( hasSource ) :
-  # print ( rtx . recognize_sphinx ( audio ) )
-#   print ( rtx . recognize_google ( audio ) )
-  # -- okay, google is good
-  # print ( rtx . recognize_google_cloud ( audio ) )
-  # print ( rtx . recognize_bing ( audio ) ) -- 需要key
-  # print ( rtx . recognize_houndify ( audio ) ) 需要client_id及client_key
-  # print ( rtx . recognize_ibm ( audio ) ) -- 需要帳戶密碼
-  # print ( rtx . recognize_wit ( audio ) ) -- 需要key
-# else :
-#   print ( "No source" )
-
 FromSource = "File"
 AudioFile  = ""
+OutputFile = ""
+Language   = "en-US"
 
 def SayHelp ( ) :
   print ( "VoiceRecognition.py"
           " -v (--help Help)"
           " -f (--from MIC or File)"
-          " -i (--input AudioFile)" )
+          " -i (--input AudioFile)"
+          " -o (--output OutputFile)"
+          " -l (--language Language)"
+          )
 
 def GetOptions ( argv ) :
   global FromSource
   global AudioFile
+  global OutputFile
+  global Language
   try :
-    opts, args = getopt.getopt(argv,"i:f:v",["input=","from=","help"])
+    opts, args = getopt . getopt (
+                   argv          ,
+                   "i:o:f:l:v"   ,
+                   [ "input="    ,
+                     "output="   ,
+                     "language=" ,
+                     "from="     ,
+                     "help"    ] )
   except getopt . GetoptError :
     SayHelp ( )
     sys . exit ( 2 )
@@ -82,6 +50,10 @@ def GetOptions ( argv ) :
       sys . exit ( 0 )
     elif opt in ("-i", "--input"):
       AudioFile  = arg
+    elif opt in ("-o", "--output"):
+      OutputFile = arg
+    elif opt in ("-l", "--language"):
+      Language   = arg
     elif opt in ("-f", "--from"):
       FromSource  = arg
   return True
@@ -101,7 +73,7 @@ if __name__ == '__main__':
     Running = True
     while Running :
       R . UseMicrophone ( )
-      line = R . Listen ( )
+      line = R . Listen ( Language )
       if ( len ( line ) > 0 ) :
         print ( line )
         line = line . strip ( )
@@ -111,4 +83,9 @@ if __name__ == '__main__':
     print ( "Program stopped" )
   else :
     R . OpenFile ( AudioFile )
-    print ( R . Listen ( ) )
+    line = R . Listen ( Language )
+    print ( line )
+    if ( len ( OutputFile ) > 0 ) :
+      if ( len ( line ) > 0 ) :
+        with open ( OutputFile , "w" ) as voiceFile :
+          voiceFile . write ( line )

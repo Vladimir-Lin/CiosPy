d1b6ae90ecba87fb7ce216a285004049373e76a8
foxman <lin.vladimir@gmail.com>
2019-12-02 06:05:37 +0800

    Commit CiosPy

--- Commit.bat
+++ Commit.bat
@@ -1,40 +1,40 @@
 D:
-pause
+
 cd D:\Programs\Python\37\Lib\CIOS
 git commit -a -m "Commit Python PyCios"
 git push
-pause
+
 cd D:\CIOS\CiosPy
 git commit -a -m "Commit CiosPy"
 git push
-pause
+
 cd D:\CIOS\WWW\ERP\www\assets\js\CIOS
 git commit -a -m "Commit Cios Javascript"
 git push
-pause
+
 cd D:\CIOS\WWW\ERP\php\vendor\foxman\phpcios
 git commit -a -m "Commit PHP Cios"
 git push
-pause
+
 cd D:\CIOS\WWW\ERP
 git commit -a -m "Commit Action ERP Web Site"
 git push
-pause
+
 cd D:\Programs\PyCpuProfiler
 git commit -a -m "Commit Action ERP Web Site"
 git push
-pause
+
 cd D:\CIOS\Libraries\Data\UUIDs
 git commit -a -m "Commit UUIDs"
 git push
-pause
+
 cd D:\CIOS\Libraries\Data\CiosTime
 git commit -a -m "Commit CIOS Time"
 git push
-pause
+
 cd D:\CIOS\Libraries\Tools\CiosParallel
 git commit -a -m "Commit CIOS Parallel"
 git push
-pause
+
 echo "Completed Update"
 exit
